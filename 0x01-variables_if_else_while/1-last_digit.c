92754dd26e7f9abc801f0651e66d026ada99804b
elmouftakir <abderahim.mouftakir@gmail.com>
2023-06-17 09:09:11 -0700

    variables_if_else_while

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,7 +7,7 @@
  * main - Entry point
  *
  * Description: print the last digit of the number n status :
- * 		greater than 5 ,is 0 and not less than 6.
+ * greater than 5 ,is 0 and not less than 6.
  *
  * Return: 0 (success)
  */
@@ -23,8 +23,8 @@ int main(void)
 	if (digit > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, digit);
 	else if (digit == 0)
-		printf("Last digit of %d is %d and is 0\n ", n, digit);
+		printf("Last digit of %d is %d and is 0\n", n, digit);
 	else if (digit < 6 && digit != 0)
-		printf("Last digit of %d is %d  and is less than 6 and not 0\n", n, digit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, digit);
 	return (0);
 }
