61950ab80ca989a6880812d91d5117d3bd39dfb0
elmouftakir <abderahim.mouftakir@gmail.com>
2023-06-18 09:37:11 -0700

    print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entre point
+ *
+ *
+ * Return: (0) is successful
+*/
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+
+	{
+		putchar(c);
+		c++;
+	{
+	c = 'A';
+	while (c <= 'Z')
+	}
+		putchar(c);
+			c++;
+	}
+	putchar('\n');
+
+	return (0);
+}
