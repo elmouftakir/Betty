82c984a8ec13a7b70d69aa0e48b2578fe0cb4da1
elmouftakir <abderahim.mouftakir@gmail.com>
2023-06-18 13:52:50 -0700

    print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entre point
+ *
+ *
+ * Return: (0) is successful
+*/
+
+int main(void)
+{
+	char c = 'z';
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+	putchar('\n');
+	return (0);
+}
