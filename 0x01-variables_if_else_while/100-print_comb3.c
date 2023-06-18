5cf09c3945fffcdc90457baa4b0c16309061aaf7
elmouftakir <abderahim.mouftakir@gmail.com>
2023-06-18 15:28:49 -0700

    print_comb

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entre point
+ *
+ *
+ * Return: (0) is successful
+*/
+
+int main(void)
+{
+	int a = '0';
+	int b = '1';
+
+	while (a <= '8')
+	{
+		b = a + 1;
+		{
+			while (b <= '9')
+			putchar(a);
+			putchar(b);
+			if (a != '8')
+				{
+					putchar(',')
+					putchar(' ')
+				}
+				b++;
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
