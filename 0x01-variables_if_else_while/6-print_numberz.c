0f5de50be0ee417543b6f9d90e92ba75e88757be
elmouftakir <abderahim.mouftakir@gmail.com>
2023-06-18 13:27:00 -0700

    print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entre point
+ *
+ *
+ * Return: (0) is successful
+*/
+
+int main(void)
+{
+	int c = '48';
+
+	while (c <= '57')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
