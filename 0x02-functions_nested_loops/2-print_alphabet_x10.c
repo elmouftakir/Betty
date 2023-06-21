c5e5e9dca1d00055b564a7578445174746b5904c
elmouftakir <abderahim.mouftakir@gmail.com>
2023-06-20 20:02:22 -0700

    2-print_alphabe

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,7 +4,7 @@
  *
  * print_alphabet_x10 - Prints the alphabet in 10 times
  *
- */
+*/
 void print_alphabet_x10(void)
 {
 	int l, c;
@@ -12,9 +12,9 @@ void print_alphabet_x10(void)
 	for (l = 0; l < 10; l++)
 	{
 		for (c = 'a'; c <= 'z'; c++)
-	{
-		_putchar(c);
-	}
-	_putchar('\n');
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
 	}
 }
