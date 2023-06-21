7d2270fe70e56f29e7afcf716be979acb8711c66
elmouftakir <abderahim.mouftakir@gmail.com>
2023-06-20 19:30:53 -0700

    2-print_alphabe

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet 10 times
+ *
+*/
+
+void print_alphabet(void)
+{
+	int line. c;
+
+	for (line = 0; line <= 9; line++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+	}
+}
