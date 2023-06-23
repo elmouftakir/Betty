2407f8285cce8e475e524514dfebc15193886804
elmouftakir <abderahim.mouftakir@gmail.com>
2023-06-23 14:36:03 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @ch: the number to be checked
+ * retourn: 1 for upper or 0 for any else
+ */
+
+int _isupper(int ch)
+
+{
+	if (ch >= 65 && ch <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
