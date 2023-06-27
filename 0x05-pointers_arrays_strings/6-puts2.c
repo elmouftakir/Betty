88338bf46790c66bb5423266a05cd9d5b26b4012
elmouftakir <abderahim.mouftakir@gmail.com>
2023-06-27 14:17:01 -0700

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * puts2 - function prints every other character of a string
+ * 
+ * @str: input
+ *
+ * Return: void
+ *
+ */
+
+void puts2(char *str)
+
+{
+
+	int lon = 0;
+	int temp = 0;
+	char *p = str;
+	int c;
+	while (*p != '\0')
+	{	p++;
+		lon++;
+	}
+	temp = lon - 1;
+	for (c = 0 ; c <= temp ; c++)
+	{
+		if (c % 2 == 0)
+		{
+			_putchar(str[c]);
+		}
+	}
+	_putchar('\n');
+}
