395329ea5e9cde1beadacf9090e5e81fa2a804e8
elmouftakir <abderahim.mouftakir@gmail.com>
2023-06-27 17:05:57 -0700

    array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -4,15 +4,14 @@
 /**
  * print_array - function that prints n elements of an array
  *
- * @a: array name
+ * @a: array
  *
- * @n: is the number of elements OF the array to be printed
+ * @n: elements of the array to be printed
  *
  * Return: a and n inputs
  */
 
 void print_array(int *a, int n)
-
 {
 	int i;
 	
