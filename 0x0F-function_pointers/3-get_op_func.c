c50ffb3345165001d2c10d2256b3e8239156a01f
elmouftakir <abderahim.mouftakir@gmail.com>
2023-07-20 02:21:44 -0700

    correct function to perform the operation asked by the user

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -20,12 +20,11 @@ int (*get_op_func(char *s))(int, int)
 	int i;
 
 	i = 0;
-	while (ops[i].op != NULL)
-	{
-		if (s[0] == ops[i].op[0] && s[1] == '\0')
-		{
-			return (ops[i].f);
 
+	while (ops[i].op)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+			return (ops[i].f);
 		i++;
 	}
 
