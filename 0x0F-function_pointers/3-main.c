c9c5f8e3a8e13ce0a8c471eca18885e5cd5ad4c3
elmouftakir <abderahim.mouftakir@gmail.com>
2023-08-16 17:51:40 -0700

    program that performs simple operations

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,29 @@
+#include "3-calc.h"
+
+/**
+ * main - program that perfroms simple operations
+ * @acgc: number of arguments
+ * @argv: array of arguments
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int n1 = 0, n2 = 0, num = 0;
+	char op;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[3]);
+	op = argv[2][0];
+
+	if (argv[2][1] != '\0' || (op != '+' && op != '-' &&
+		op != '/' && op != '*' && op != '%'))
+	{
+		printf("Error\n");
+		exit(99);
+	}
