3b794fd301523e701c4a1fd33723166a37957aa6
elmouftakir <abderahim.mouftakir@gmail.com>
2023-09-04 17:07:12 -0700

    function that reads a text file and prints it to the POSIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,45 +1,36 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: name of the file to read
- * @letters: number of letters it should read and print
- * Return: number of letters it could read and print
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: numbers of letters printed. It fails, returns 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int f;
-	char *b;
-	ssize_t read_bytes, write_bytes;
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
 
-	b = malloc(sizeof(char) * letters);
-	if (b == NULL || filename == NULL)
+	if (!filename)
 		return (0);
 
-	f = open(filename, O_RDONLY);
-	if (f == -1)
-	{
-		free(b);
-		return (0);
-	}
+	fd = open(filename, O_RDONLY);
 
-	read_bytes = read(f, b, letters);
-	if (read_bytes == -1)
-	{
-		free(b);
-		close(f);
+	if (fd == -1)
 		return (0);
-	}
 
-	write_bytes = write(STDOUT_FILENO, b, read_bytes);
-	if (write_bytes == -1)
-	{
-		free(b);
-		close(f);
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
 		return (0);
-	}
 
-	free(b);
-	close(f);
-	return (write_bytes);
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+
+	close(fd);
+
+	free(buf);
+
+	return (nwr);
 }
